c83850e46b25f53fb3d5524dd53deb37ce20614c
Bhawana Bisht <bhawanab2003@gmail.com>
2026-08-01 21:18:35 +0530

    Add longestKSubstr function implementation
    Implement longestKSubstr function to find the longest substring with exactly k unique characters.

---
+++ longest_k_substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // code here
+        int low=0; 
+        int high =0;
+        int res=INT_MIN;
+        int n= s.size();
+         unordered_map<char, int>f;
+         for (int high = 0; high < n; high++) {
+    f[s[high]]++;
+
+    while (f.size() > k) {
+        f[s[low]]--;
+        if (f[s[low]] == 0)
+            f.erase(s[low]);
+        low++;
+    }
+
+    if (f.size() == k) {
+        int len = high - low + 1;
+        res = max(res, len);
+    }
+}
+             if(res==INT_MIN)
+              return -1;
+               return res;
+    }
+          
+    
+};
